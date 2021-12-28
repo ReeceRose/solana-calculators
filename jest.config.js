2091e625c03a07655da0b5342aeb3e69fa319fec
Reece Rose <reecerose18@gmail.com>
2021-12-28 11:27:11 -0500

    Removed unused dev dep. Upgraded to latest packages. Code cleanup

--- jest.config.js
+++ jest.config.js
@@ -12,8 +12,6 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/.jest/setupTests.js'],
   testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
   transform: {
-    // Use babel-jest to transpile tests with the next/babel preset
-    // https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object
     '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
   },
   transformIgnorePatterns: [
